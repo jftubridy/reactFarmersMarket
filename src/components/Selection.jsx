625d3e00ede7fef5cfd8a05863883609bb395093
Joseph Tubridy and Sharon Lee <sharon@>
2020-02-12 13:51:13 -0800

    add fake hidden produce by utilizing color change

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -1,36 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-let hovered = true;
+// let hovered = true;
+
+// function Selection(props){
+//   const hideStyle = {
+//     color: 'red'
+//   }
+//   const showStyle = {
+//     color: 'blue'
+//   }
+//   const toggleHover = () => {
+//     console.log("Hovered: ", hovered);
+//     hovered = !hovered;
+//   }
+//   return(
+//     <div>
+//       <h3 onMouseEnter = {toggleHover} style = {hovered ? hideStyle : showStyle}>{props.month}</h3>
+//       <ul>
+//         <li><style jsx>{`
+//         li {
+           
+//            }
+//         li :hover {
+//           visibility: visible;
+//            }
+//         `}</style>{props.selection} 
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+// ---------This commented out code is the one that actually toggles when hovering the Months, however hiding/showing it not functional yet
 
 function Selection(props){
-  const hideStyle = {
-    color: 'red'
-  }
-  const showStyle = {
-    color: 'blue'
-  }
-  const toggleHover = () => {
-    console.log("Hovered: ", hovered);
-    hovered = !hovered;
-  }
   return(
     <div>
-      <h3 onMouseEnter = {toggleHover} style = {hovered ? hideStyle : showStyle}>{props.month}</h3>
+      <h3>{props.month}</h3>
       <ul>
         <li><style jsx>{`
-        li {
-           
-           }
-        li :hover {
-          visibility: visible;
-           }
-        `}</style>{props.selection} 
-        </li>
+        li{
+          color: white
+        }
+        li:hover {
+          color: black
+        }
+      `}</style>{props.selection}</li>
       </ul>
     </div>
   );
 }
+//       {/* -------This code is faking hidden */}
+
+// function Selection(props){
+//   return(
+//     <div>
+//       <h3 id="a"><style jsx>{`
+//         ~#b {
+//           visibility: hidden
+//         }
+//         #a:hover ~#b {
+//           visibility: visible
+//         }
+//       `}</style>{props.month}</h3>
+//       <ul>
+//         <li id="b">{props.selection}</li>
+//       </ul>
+//     </div>
+//   );
+// }  
+// ------This code is trying to use general sibling selector (~) to use h3 to effect li, unsuccessful.
 
 Selection.propTypes={
   month: PropTypes.string.isRequired,
@@ -39,11 +78,4 @@ Selection.propTypes={
 
 export default Selection;
 
- <style jsx>{`
-div {
-  font-size: 0;
-}
-div :hover {
-  font-size: 12;
-}
-`}</style> 
+
